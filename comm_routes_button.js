48bf5f285723e1a7879fca17fc148f6540f01c18
ellie <yaeleiger@brandeis.edu>
2019-01-26 16:50:23 -0500

    Update comm_routes_button.js

--- comm_routes_button.js
+++ comm_routes_button.js
@@ -21,5 +21,5 @@ class LikeButton extends React.Component {
     );
   }
 }
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector('#comm_routes_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
